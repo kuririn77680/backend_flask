14daff466fc7ffd0ea1d3686c9c899c1f295f06a
Florian  GAGNON <florian-gagnon@aviss-securite.fr>
2022-05-13 12:27:36 +0200

    initial commit

---
+++ backend/routes/error.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, jsonify
+from werkzeug.exceptions import NotFound
+
+error_bp = Blueprint("errors", __name__)
+
+
+@error_bp.app_errorhandler(NotFound)
+def handle_not_found(err):
+    return jsonify({"message": "This resource isn't available."}), 500
+
+
+@error_bp.app_errorhandler(Exception)
+def handle_generic_exception(err):
+    return jsonify({"message": "Unknown error. Please check the logs for more details."}), 500
+
