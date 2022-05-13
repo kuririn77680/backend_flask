48d921464304a05c60bd7757235fd3e62b673578
Florian  GAGNON <florian-gagnon@aviss-securite.fr>
2022-05-13 12:36:40 +0200

    fix blueprint error name

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, Response, request
 
-users_bp = Blueprint("health", __name__, url_prefix="/users")
+users_bp = Blueprint("users", __name__, url_prefix="/users")
 
 
 @users_bp.route("", methods=["GET"])
